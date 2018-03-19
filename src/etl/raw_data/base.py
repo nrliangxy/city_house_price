1d8bbbef6f3842a3d902ea7aeafcdceffeee2b0e
nrliangxy <nrliangxy@foxmail.com>
2018-03-19 09:42:49 +0800

    first commit

---
+++ src/etl/raw_data/base.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "xiaoyong.Liang"
+
+class House_price_base(object):
+    def __init__(self, item):
+        self.item = item
+        
---
+++ src/etl/raw_data/house_price_process.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "xiaoyong.Liang"
+import sys, json
+from base import House_price_base
+# print(sys.path)
+from pyquery import PyQuery as pq
+class JSONObject:
+    def __init__(self, d):
+        self.__dict__ = d
+
+# with open('04480984-2b07-11e8-8155-e09467d4fa03.json', 'r') as f:
+#     data = json.load(f, object_hook=JSONObject)
+    # data = json.loads(f.readlines(), object_hook=JSONObject)
+# print(data.data_type)
+class New_house_process(object):
+    def __init__(self, item):
+        # super().__init__(item)
+        self.item = item
+        self.obj = None
+        
+        if item:
+            self.obj = self.load()
+    
+    def load(self):
+        with open(self.item, 'r') as f:
+            return json.load(f, object_hook=JSONObject)
+    
+    
+    def process(self):
+        print(self.obj.data_type)
+        
+obj = New_house_process('04480984-2b07-11e8-8155-e09467d4fa03.json')
+obj.process()
