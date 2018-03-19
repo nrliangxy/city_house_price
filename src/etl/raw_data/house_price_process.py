42be1a2eb6f0211f075ea6d1d4d6bb7e4a299ace
nrliangxy <nrliangxy@foxmail.com>
2018-03-19 09:52:57 +0800

    update

--- src/etl/raw_data/house_price_process.py
+++ src/etl/raw_data/house_price_process.py
@@ -10,10 +10,13 @@ class JSONObject:
     def __init__(self, d):
         self.__dict__ = d
 
+
 # with open('04480984-2b07-11e8-8155-e09467d4fa03.json', 'r') as f:
 #     data = json.load(f, object_hook=JSONObject)
     # data = json.loads(f.readlines(), object_hook=JSONObject)
 # print(data.data_type)
+
+
 class New_house_process(object):
     def __init__(self, item):
         # super().__init__(item)
